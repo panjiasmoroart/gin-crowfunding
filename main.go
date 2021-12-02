a6067adeba90e468e99c633c3818b9bb99c2a659
panjiasmoroart <pandjiasmoroart@gmail.com>
2021-12-02 13:42:45 +0700

    Fundamental - 3. Gorm get table data

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"gin-crowfunding/user"
 	"log"
 
 	"gorm.io/driver/mysql"
@@ -10,11 +11,26 @@ import (
 
 func main() {
 	dsn := "panjiasmoro:panjiasmoro090949@tcp(127.0.0.1:3306)/crowfunding?charset=utf8mb4&parseTime=True&loc=Local"
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
 	fmt.Println("Connection to database is success")
+
+	var users []user.User
+	length := len(users)
+	fmt.Println(length)
+
+	db.Find(&users)
+
+	length = len(users)
+	fmt.Println(length)
+
+	for _, user := range users {
+		fmt.Println(user.Name)
+		fmt.Println(user.Email)
+		fmt.Println("================")
+	}
 }
