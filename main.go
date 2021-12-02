cddfe2ac3f9ac8ebc5ba7f7aeea23f5240c291e8
panjiasmoroart <pandjiasmoroart@gmail.com>
2021-12-02 13:17:18 +0700

    Fundamental - 2. Gorm connection to db

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "panjiasmoro:panjiasmoro090949@tcp(127.0.0.1:3306)/crowfunding?charset=utf8mb4&parseTime=True&loc=Local"
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	fmt.Println("Connection to database is success")
+}
