8b53401a7e63f1cfc8ec7e2925296e665498e9e0
panjiasmoroart <pandjiasmoroart@gmail.com>
2021-12-02 14:53:52 +0700

    Register User Endpoint - 1. Register User - Repository

--- main.go
+++ main.go
@@ -3,39 +3,12 @@ package main
 import (
 	"gin-crowfunding/user"
 	"log"
-	"net/http"
 
-	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
-	// dsn := "panjiasmoro:panjiasmoro090949@tcp(127.0.0.1:3306)/crowfunding?charset=utf8mb4&parseTime=True&loc=Local"
-	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
-	// if err != nil {
-	// 	log.Fatal(err.Error())
-	// }
-
-	// fmt.Println("Connection to database is success")
-
-	// var users []user.User
-
-	// db.Find(&users)
-
-	// for _, user := range users {
-	// 	fmt.Println(user.Name)
-	// 	fmt.Println(user.Email)
-	// 	fmt.Println("================")
-	// }
-
-	router := gin.Default()
-	router.GET("/handler", handler)
-	router.Run()
-}
-
-func handler(c *gin.Context) {
 	dsn := "panjiasmoro:panjiasmoro090949@tcp(127.0.0.1:3306)/crowfunding?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
@@ -43,13 +16,10 @@ func handler(c *gin.Context) {
 		log.Fatal(err.Error())
 	}
 
-	var users []user.User
-	db.Find(&users)
+	userRepository := user.NewRepository(db)
+	user := user.User{
+		Name: "Test simpan",
+	}
 
-	c.JSON(http.StatusOK, users)
-
-	// handler
-	// service
-	// repository
-	// db
+	userRepository.Save(user)
 }
