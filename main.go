8555959a55bef3b20f61b57793955e9e988de580
panjiasmoroart <pandjiasmoroart@gmail.com>
2022-03-08 23:23:00 +0700

    JWT Auth - 8. JWT Auth -  Test validate token

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"gin-crowfunding/auth"
 	"gin-crowfunding/handler"
 	"gin-crowfunding/user"
@@ -26,6 +27,24 @@ func main() {
 	// testing generate token
 	authService := auth.NewService()
 	// fmt.Println(authService.GenerateToken(1001))
+
+	// testing validate token
+	token, err := authService.ValidateToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxfQ.JRouLhzpLqMitGBt8IEkgZOAHlRt9umx4RCl6Oiiijw")
+	if err != nil {
+		fmt.Println("ERROR")
+		fmt.Println("ERROR")
+		fmt.Println("ERROR")
+	}
+
+	if token.Valid {
+		fmt.Println("VALID")
+		fmt.Println("VALID")
+		fmt.Println("VALID")
+	} else {
+		fmt.Println("INVALID")
+		fmt.Println("INVALID")
+		fmt.Println("INVALID")
+	}
 
 	// test upload file masih di hardcode dulu sementara
 	// userService.SaveAvatar(1, "images/Img-Profil.jpg")
