4d96cea7f5c60ccbda6114d1e66212dcea8e7060
panjiasmoroart <pandjiasmoroart@gmail.com>
2022-03-09 18:59:53 +0700

    Auth Middleware - 4. Middleware part 2

--- main.go
+++ main.go
@@ -32,22 +32,22 @@ func main() {
 	// fmt.Println(authService.GenerateToken(1001))
 
 	// testing validate token
-	token, err := authService.ValidateToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxfQ.JRouLhzpLqMitGBt8IEkgZOAHlRt9umx4RCl6Oiiijw")
-	if err != nil {
-		fmt.Println("ERROR")
-		fmt.Println("ERROR")
-		fmt.Println("ERROR")
-	}
+	// token, err := authService.ValidateToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxfQ.JRouLhzpLqMitGBt8IEkgZOAHlRt9umx4RCl6Oiiijw")
+	// if err != nil {
+	// 	fmt.Println("ERROR")
+	// 	fmt.Println("ERROR")
+	// 	fmt.Println("ERROR")
+	// }
 
-	if token.Valid {
-		fmt.Println("VALID")
-		fmt.Println("VALID")
-		fmt.Println("VALID")
-	} else {
-		fmt.Println("INVALID")
-		fmt.Println("INVALID")
-		fmt.Println("INVALID")
-	}
+	// if token.Valid {
+	// 	fmt.Println("VALID")
+	// 	fmt.Println("VALID")
+	// 	fmt.Println("VALID")
+	// } else {
+	// 	fmt.Println("INVALID")
+	// 	fmt.Println("INVALID")
+	// 	fmt.Println("INVALID")
+	// }
 
 	// test upload file masih di hardcode dulu sementara
 	// userService.SaveAvatar(1, "images/Img-Profil.jpg")
@@ -60,33 +60,37 @@ func main() {
 	api.POST("/users", userHandler.RegisterUser)
 	api.POST("/sessions", userHandler.Login)
 	api.POST("/email_checkers", userHandler.CheckEmailAvailability)
-	api.POST("/avatars", userHandler.UploadAvatar)
+	api.POST("/avatars", authMiddleware(authService, userService), userHandler.UploadAvatar)
 
 	router.Run()
 }
 
-// middleware sesuatu yang ada ditengah-tengah antara user dan request userHandler
-func authMiddleware(c *gin.Context) {
-	// ambil nilai header Authorization: Bearer tokentoken
-	authHeader := c.GetHeader("Authorization")
+func authMiddleware(authService auth.Service, userService user.Service) gin.HandlerFunc {
+	// middleware sesuatu yang ada ditengah-tengah antara user dan request userHandler
+	return func (c *gin.Context) {
+		// ambil nilai header Authorization: Bearer tokentoken
+		authHeader := c.GetHeader("Authorization")
 
-	if !strings.Contains(authHeader, "Bearer") {
-		response := helper.APIResponse("Unauthorized", http.StatusUnauthorized, "error", nil)
-		c.AbortWithStatusJSON(http.StatusUnauthorized, response)
-		return
+		if !strings.Contains(authHeader, "Bearer") {
+			response := helper.APIResponse("Unauthorized", http.StatusUnauthorized, "error", nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+
+		// dari header Authorization, ambil tokennya saja
+		// Bearer tokentoken
+		tokenString := ""
+		arrayToken := strings.Split(authHeader, " ")
+		if len(arrayToken == 2) {
+			tokenString = arrayToken[1]
+		}
+
+		token, err :=
+
 	}
-
-	// dari header Authorization, ambil tokennya saja
-	// Bearer tokentoken
-	tokenString := ""
-	arrayToken := strings.Split(authHeader, " ")
-	if len(arrayToken == 2) {
-		tokenString = arrayToken[1]
-	}
-
-	token, err :=
 
 }
+
 
 // kita validasi token, kalo valid kita ambil user_id
 // ambil user dari db berdasarkan user_id lewat service
