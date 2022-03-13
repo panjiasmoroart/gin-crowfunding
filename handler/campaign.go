bfd2429e20ce4eceda43362c8fac73e9e68275cb
panjiasmoroart <pandjiasmoroart@gmail.com>
2022-03-13 09:27:43 +0700

    Create campaign endpoint - 1. Analisis

--- handler/campaign.go
+++ handler/campaign.go
@@ -58,3 +58,9 @@ func (h *campaignHandler) GetCampaign(c *gin.Context) {
 	response := helper.APIResponse("Campaign detail", http.StatusOK, "success", campaign.FormatCampaignDetail(campaignDetail))
 	c.JSON(http.StatusOK, response)
 }
+
+// langkah-langkah create campaign
+// tangkap parameter dari user ke input struct
+// ambil current user dari jwt/handler
+// panggil service, parameternya input struct yg telah dimapping
+// panggil repository untuk simpan data campaign baru
