236a9b10f9accdec790e3ca3049ef4e35b0c005b
panjiasmoroart <pandjiasmoroart@gmail.com>
2022-03-13 09:45:08 +0700

    Create campaign endpoint - 3. Service Part 1

--- campaign/input.go
+++ campaign/input.go
@@ -1,6 +1,18 @@
 package campaign
 
+import "gin-crowfunding/user"
+
 type GetCampaignDetailInput struct {
 	// ada beberapa cara mengirim parameter, json, query params dan uri
 	ID int `uri:"id" binding:"required"`
+}
+
+// untuk user kita tidak ambil datanya dari json melainkan middleware
+type CreateCampaignInput struct {
+	Name             string `json:"name"`
+	ShortDescription string `json:"short_description"`
+	Description      string `json:"description"`
+	GoalAmount       int    `json:"goal_amount"`
+	Perks            string `json:"perks"`
+	User             user.User
 }
