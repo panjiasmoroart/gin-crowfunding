dbde0165d2f362958cdecc18bda945f7b8209994
panjiasmoroart <pandjiasmoroart@gmail.com>
2022-03-15 17:24:35 +0700

    Upload campaign image endpoint - 5. Handler

--- handler/campaign.go
+++ handler/campaign.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"fmt"
 	"gin-crowfunding/campaign"
 	"gin-crowfunding/helper"
 	"gin-crowfunding/user"
@@ -146,3 +147,49 @@ func (h *campaignHandler) UpdateCampaign(c *gin.Context) {
 // repository
 // 1. create image/save ke dalam table campaign_images
 // 2. ubah is_primary 1 (true) ke false jika sebelumnya sudah ada is_primary 1
+func (h *campaignHandler) UploadImage(c *gin.Context) {
+	var input campaign.CreateCampaignImageInput
+
+	err := c.ShouldBind(&input)
+	if err != nil {
+		response := helper.APIResponse("Failed to upload campaign image", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	file, err := c.FormFile("file")
+	if err != nil {
+		data := gin.H{"is_uploaded": false}
+		response := helper.APIResponse("Failed to upload campaign image", http.StatusBadRequest, "error", data)
+
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	currentUser := c.MustGet("currentUser").(user.User)
+	userID := currentUser.ID
+
+	path := fmt.Sprintf("images/%d-%s", userID, file.Filename)
+	err = c.SaveUploadedFile(file, path)
+	if err != nil {
+		data := gin.H{"is_uploaded": false}
+		response := helper.APIResponse("Failed to upload campaign image", http.StatusBadRequest, "error", data)
+
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	_, err = h.service.SaveCampaignImage(input, path)
+	if err != nil {
+		data := gin.H{"is_uploaded": false}
+		response := helper.APIResponse("Failed to upload campaign image", http.StatusBadRequest, "error", data)
+
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	data := gin.H{"is_uploaded": true}
+	response := helper.APIResponse("Campaign image successfuly uploaded", http.StatusOK, "success", data)
+
+	c.JSON(http.StatusOK, response)
+}
