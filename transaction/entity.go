6397b72c7779b19ebdcccd746f1f03fd53381f9b
panjiasmoroart <pandjiasmoroart@gmail.com>
2022-03-17 23:04:09 +0700

    Transaction Persiapan - 3. Entity and 4. Recap

--- transaction/entity.go
+++ transaction/entity.go
@@ -1,1 +1,15 @@
 package transaction
+
+import "time"
+
+// Representasi dari table transactions
+type Transaction struct {
+	ID         int
+	CampaignID int
+	UserID     int
+	Amount     int
+	Status     string
+	Code       string
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+}
