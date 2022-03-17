428257be0187826eccb06b4595cead32cba1d2f5
panjiasmoroart <pandjiasmoroart@gmail.com>
2022-03-17 14:56:11 +0700

    Upload campaign image endpoint - 9. Reacap & commit and 10. image - fixed bug

--- campaign/input.go
+++ campaign/input.go
@@ -19,7 +19,7 @@ type CreateCampaignInput struct {
 
 type CreateCampaignImageInput struct {
 	CampaignID int `form:"campaign_id" binding:"required"`
-	// remove required
+	// remove required fix bug
 	IsPrimary bool `form:"is_primary"`
 	User      user.User
 }
