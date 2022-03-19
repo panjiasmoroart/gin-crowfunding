aa706236a77f4b0b0ef4b9d7d5222142cdcc8df6
panjiasmoroart <pandjiasmoroart@gmail.com>
2022-03-19 22:08:11 +0700

    Campaign Transaction Endpoint - 1. Analisis

--- handler/transaction.go
+++ handler/transaction.go
@@ -1,1 +1,7 @@
 package handler
+
+// parameter di uri
+// tangkap parameter mapping input struct
+// panggil service, input struct sebagai parameter
+// service, berbekal campaign id panggil repo
+// repo mencari data transcation suatu campaign
