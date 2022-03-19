a8e208add9a03d1135ec092131d1244e7208f0c3
panjiasmoroart <pandjiasmoroart@gmail.com>
2022-03-19 22:36:05 +0700

    Campaign Transaction Endpoint - 3. Service

--- transaction/service.go
+++ transaction/service.go
@@ -1,1 +1,22 @@
 package transaction
+
+type Service interface {
+	GetTransactionsByCampaignID(camapignID int) ([]Transaction, error)
+}
+
+type service struct {
+	repository Repository
+}
+
+func NewService(repository Repository) *service {
+	return &service{repository}
+}
+
+func (s *service) GetTransactionsByCampaignID(camapignID int) ([]Transaction, error) {
+	transactions, err := s.repository.GetByCampaignID(camapignID)
+	if err != nil {
+		return transactions, err
+	}
+
+	return transactions, nil
+}
