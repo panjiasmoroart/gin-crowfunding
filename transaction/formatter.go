cab95e58526cd9b958890bffa6e431d8c0cc6417
panjiasmoroart <pandjiasmoroart@gmail.com>
2022-03-23 17:06:37 +0700

    Campaign Transaction Endpoint - 8. Formatter

--- transaction/formatter.go
+++ transaction/formatter.go
@@ -1,1 +1,34 @@
 package transaction
+
+import "time"
+
+type CampaignTransactionFormatter struct {
+	ID        int       `json:"id"`
+	Name      string    `json:"name"`
+	Amount    int       `json:"amount"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+// untuk ngeformat 1 data transcation
+func FormatCampaignTransaction(transaction Transaction) CampaignTransactionFormatter {
+	formatter := CampaignTransactionFormatter{}
+	formatter.ID = transaction.ID
+	formatter.Name = transaction.User.Name
+	formatter.Amount = transaction.Amount
+	formatter.CreatedAt = transaction.CreatedAt
+	return formatter
+}
+
+func FormatCampaignTransactions(transactions []Transaction) []CampaignTransactionFormatter {
+	if len(transactions) == 0 {
+		return []CampaignTransactionFormatter{}
+	}
+
+	var transactionsFormatter []CampaignTransactionFormatter
+	for _, transcation := range transactions {
+		formatter := FormatCampaignTransaction(transcation)
+		transactionsFormatter = append(transactionsFormatter, formatter)
+	}
+
+	return transactionsFormatter
+}
